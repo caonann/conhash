c9ef2f67117df6ecc58f3d2df8c92e303fc3107c
Sephirothv <546314485@qq.com>
2017-10-25 23:43:51 +0800

    conhash
    conhash

---
+++ src/CMakeFiles/conhash.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/conhash.dir/conhash.cpp.o"
+  "CMakeFiles/conhash.dir/md5.c.o"
+  "libconhash.pdb"
+  "libconhash.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/conhash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
